b404fe8d676dacedd160c6bfd3612f8b5dab3dd0
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2020-11-24 15:02:21 -0700

    Imported auxiliary library functions and enums from hipBLAS
    [ROCm/hipSOLVER commit: d58d45c31d72bfab78cd771195e32f1e3284c609]

--- library/include/hipsolver.h
+++ library/include/hipsolver.h
@@ -18,4 +18,179 @@
 #include <hip/hip_runtime_api.h>
 #include <stdint.h>
 
+/* Workaround clang bug:
+
+   https://bugs.llvm.org/show_bug.cgi?id=35863
+
+   This macro expands to static if clang is used; otherwise it expands empty.
+   It is intended to be used in variable template specializations, where clang
+   requires static in order for the specializations to have internal linkage,
+   while technically, storage class specifiers besides thread_local are not
+   allowed in template specializations, and static in the primary template
+   definition should imply internal linkage for all specializations.
+
+   If clang shows an error for improperly using a storage class specifier in
+   a specialization, then HIPSOLVER_CLANG_STATIC should be redefined as empty,
+   and perhaps removed entirely, if the above bug has been fixed.
+*/
+#if __clang__
+#define HIPSOLVER_CLANG_STATIC static
+#else
+#define HIPSOLVER_CLANG_STATIC
+#endif
+
+typedef void* hipsolverHandle_t;
+
+typedef struct hipsolverComplex
+{
+#ifndef __cplusplus
+
+    float x, y;
+
+#else
+
+private:
+    float x, y;
+
+public:
+#if __cplusplus >= 201103L
+    hipsolverComplex() = default;
+#else
+    hipsolverComplex() {}
+#endif
+
+    hipsolverComplex(float r, float i = 0)
+        : x(r)
+        , y(i)
+    {
+    }
+
+    float real() const
+    {
+        return x;
+    }
+    float imag() const
+    {
+        return y;
+    }
+    void real(float r)
+    {
+        x = r;
+    }
+    void imag(float i)
+    {
+        y = i;
+    }
+
+#endif
+} hipsolverComplex;
+
+typedef struct hipsolverDoubleComplex
+{
+#ifndef __cplusplus
+
+    double x, y;
+
+#else
+
+private:
+    double x, y;
+
+public:
+
+#if __cplusplus >= 201103L
+    hipsolverDoubleComplex() = default;
+#else
+    hipsolverDoubleComplex() {}
+#endif
+
+    hipsolverDoubleComplex(double r, double i = 0)
+        : x(r)
+        , y(i)
+    {
+    }
+    double real() const
+    {
+        return x;
+    }
+    double imag() const
+    {
+        return y;
+    }
+    void real(double r)
+    {
+        x = r;
+    }
+    void imag(double i)
+    {
+        y = i;
+    }
+
+#endif
+} hipsolverDoubleComplex;
+
+#if __cplusplus >= 201103L
+#include <type_traits>
+static_assert(std::is_standard_layout<hipsolverComplex>{},
+              "hipsolverComplex is not a standard layout type, and thus is incompatible with C.");
+static_assert(
+    std::is_standard_layout<hipsolverDoubleComplex>{},
+    "hipsolverDoubleComplex is not a standard layout type, and thus is incompatible with C.");
+static_assert(std::is_trivial<hipsolverComplex>{},
+              "hipsolverComplex is not a trivial type, and thus is incompatible with C.");
+static_assert(std::is_trivial<hipsolverDoubleComplex>{},
+              "hipsolverDoubleComplex is not a trivial type, and thus is incompatible with C.");
+static_assert(sizeof(hipsolverComplex) == sizeof(float) * 2
+                  && sizeof(hipsolverDoubleComplex) == sizeof(double) * 2
+                  && sizeof(hipsolverDoubleComplex) == sizeof(hipsolverComplex) * 2,
+              "Sizes of hipsolverComplex or hipsolverDoubleComplex are inconsistent");
+#endif
+
+typedef enum
+{
+    HIPSOLVER_STATUS_SUCCESS           = 0, // Function succeeds
+    HIPSOLVER_STATUS_NOT_INITIALIZED   = 1, // HIPSOLVER library not initialized
+    HIPSOLVER_STATUS_ALLOC_FAILED      = 2, // resource allocation failed
+    HIPSOLVER_STATUS_INVALID_VALUE     = 3, // unsupported numerical value was passed to function
+    HIPSOLVER_STATUS_MAPPING_ERROR     = 4, // access to GPU memory space failed
+    HIPSOLVER_STATUS_EXECUTION_FAILED  = 5, // GPU program failed to execute
+    HIPSOLVER_STATUS_INTERNAL_ERROR    = 6, // an internal HIPSOLVER operation failed
+    HIPSOLVER_STATUS_NOT_SUPPORTED     = 7, // function not implemented
+    HIPSOLVER_STATUS_ARCH_MISMATCH     = 8,
+    HIPSOLVER_STATUS_HANDLE_IS_NULLPTR = 9, // hipSOLVER handle is null pointer
+} hipsolverStatus_t;
+
+// set the values of enum constants to be the same as those used in cblas
+typedef enum
+{
+    HIPSOLVER_OP_N = 111,
+    HIPSOLVER_OP_T = 112,
+    HIPSOLVER_OP_C = 113,
+} hipsolverOperation_t;
+
+typedef enum
+{
+    HIPSOLVER_FILL_MODE_UPPER = 121,
+    HIPSOLVER_FILL_MODE_LOWER = 122,
+    HIPSOLVER_FILL_MODE_FULL  = 123,
+} hipsolverFillMode_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+HIPSOLVER_EXPORT hipsolverStatus_t hipsolverCreate(hipsolverHandle_t* handle);
+
+HIPSOLVER_EXPORT hipsolverStatus_t hipsolverDestroy(hipsolverHandle_t handle);
+
+HIPSOLVER_EXPORT hipsolverStatus_t hipsolverSetStream(hipsolverHandle_t handle,
+                                                      hipStream_t       streamId);
+
+HIPSOLVER_EXPORT hipsolverStatus_t hipsolverGetStream(hipsolverHandle_t handle,
+                                                      hipStream_t*      streamId);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
--- library/src/nvcc_detail/hipsolver.cpp
+++ library/src/nvcc_detail/hipsolver.cpp
@@ -3,7 +3,112 @@
  * ************************************************************************ */
 
 #include "hipsolver.h"
-#include <cublas_v2.h>
-#include <cuda_runtime_api.h>
+#include <cuda_runtime.h>
 #include <cusolverDn.h>
 #include <hip/hip_runtime.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+cublasOperation_t hip2cuda_operation(hipsolverOperation_t op)
+{
+    switch(op)
+    {
+    case HIPSOLVER_OP_N:
+        return CUBLAS_OP_N;
+    case HIPSOLVER_OP_T:
+        return CUBLAS_OP_T;
+    case HIPSOLVER_OP_C:
+        return CUBLAS_OP_C;
+    }
+    throw "Non existent OP";
+}
+
+hipsolverOperation_t cuda2hip_operation(cublasOperation_t op)
+{
+    switch(op)
+    {
+    case CUBLAS_OP_N:
+        return HIPSOLVER_OP_N;
+    case CUBLAS_OP_T:
+        return HIPSOLVER_OP_T;
+    case CUBLAS_OP_C:
+        return HIPSOLVER_OP_C;
+    }
+    throw "Non existent OP";
+}
+
+cublasFillMode_t hip2cuda_fill(hipsolverFillMode_t fill)
+{
+    switch(fill)
+    {
+    case HIPSOLVER_FILL_MODE_UPPER:
+        return CUBLAS_FILL_MODE_UPPER;
+    case HIPSOLVER_FILL_MODE_LOWER:
+        return CUBLAS_FILL_MODE_LOWER;
+    }
+    throw "Non existent FILL";
+}
+
+hipsolverFillMode_t cuda2hip_fill(cublasFillMode_t fill)
+{
+    switch(fill)
+    {
+    case CUBLAS_FILL_MODE_UPPER:
+        return HIPSOLVER_FILL_MODE_UPPER;
+    case CUBLAS_FILL_MODE_LOWER:
+        return HIPSOLVER_FILL_MODE_LOWER;
+    }
+    throw "Non existent FILL";
+}
+
+hipsolverStatus_t cuda2hip_status(cusolverStatus_t cuStatus)
+{
+    switch(cuStatus)
+    {
+    case CUSOLVER_STATUS_SUCCESS:
+        return HIPSOLVER_STATUS_SUCCESS;
+    case CUSOLVER_STATUS_NOT_INITIALIZED:
+        return HIPSOLVER_STATUS_NOT_INITIALIZED;
+    case CUSOLVER_STATUS_ALLOC_FAILED:
+        return HIPSOLVER_STATUS_ALLOC_FAILED;
+    case CUSOLVER_STATUS_INVALID_VALUE:
+        return HIPSOLVER_STATUS_INVALID_VALUE;
+    case CUSOLVER_STATUS_MAPPING_ERROR:
+        return HIPSOLVER_STATUS_MAPPING_ERROR;
+    case CUSOLVER_STATUS_EXECUTION_FAILED:
+        return HIPSOLVER_STATUS_EXECUTION_FAILED;
+    case CUSOLVER_STATUS_INTERNAL_ERROR:
+        return HIPSOLVER_STATUS_INTERNAL_ERROR;
+    case CUSOLVER_STATUS_NOT_SUPPORTED:
+        return HIPSOLVER_STATUS_NOT_SUPPORTED;
+    case CUSOLVER_STATUS_ARCH_MISMATCH:
+        return HIPSOLVER_STATUS_ARCH_MISMATCH;
+    }
+    throw "Unimplemented status";
+}
+
+hipsolverStatus_t hipsolverCreate(hipsolverHandle_t* handle)
+{
+    return cuda2hip_status(cusolverDnCreate((cusolverDnHandle_t*)handle));
+}
+
+hipsolverStatus_t hipsolverDestroy(hipsolverHandle_t handle)
+{
+    return cuda2hip_status(cusolverDnDestroy((cusolverDnHandle_t)handle));
+}
+
+hipsolverStatus_t hipsolverSetStream(hipsolverHandle_t handle, hipStream_t streamId)
+{
+    return cuda2hip_status(cusolverDnSetStream((cusolverDnHandle_t)handle, streamId));
+}
+
+hipsolverStatus_t hipsolverGetStream(hipsolverHandle_t handle, hipStream_t* streamId)
+{
+    return cuda2hip_status(cusolverDnGetStream((cusolverDnHandle_t)handle, streamId));
+}
+
+#ifdef __cplusplus
+}
+#endif
