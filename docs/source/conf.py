1c454c5ee037e956c9b5e736ca2f5ff695d17771
jzuniga-amd <juan.zuniga-anaya@amd.com>
2021-06-11 12:41:23 -0600

    changelog and version for next dev cycle

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ if read_the_docs_build:
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.1'
+# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
