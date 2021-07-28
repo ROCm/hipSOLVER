d0069f687a28195a63ca6303eba54e57877e2b24
Juan Zuniga-Anaya <50754207+jzuniga-amd@users.noreply.github.com>
2021-07-28 16:07:09 -0600

    bump version and prepare changelog for new dev cycle (#39)
    [ROCm/hipSOLVER commit: b119128af381d8620e68c48957f027b5b27d92df]

--- bump_hipsolver_version.sh
+++ bump_hipsolver_version.sh
@@ -3,19 +3,19 @@
 # run this script in develop after merging develop/staging into master at the feature-complete date
 # Edit script to bump versions for new development cycle/release.
 
-OLD_HIPSOLVER_VERSION="1.1.0"  # Distributed with ROCm 4.5
-NEW_HIPSOLVER_VERSION="1.2.0"  # Distributed with ROCm 4.6
+OLD_HIPSOLVER_VERSION="1.2.0"  # Distributed with ROCm 5.0
+NEW_HIPSOLVER_VERSION="1.3.0"  # Distributed with ROCm 5.1
 sed -i "s/${OLD_HIPSOLVER_VERSION}/${NEW_HIPSOLVER_VERSION}/g" CMakeLists.txt
 
 # for documentation
-OLD_HIPSOLVER_DOCS_VERSION="1.1"
-NEW_HIPSOLVER_DOCS_VERSION="1.2"
+OLD_HIPSOLVER_DOCS_VERSION="1.2"
+NEW_HIPSOLVER_DOCS_VERSION="1.3"
 sed -i "s/${OLD_HIPSOLVER_DOCS_VERSION}/${NEW_HIPSOLVER_DOCS_VERSION}/g" docs/source/conf.py
 
 # for rocBLAS/rocSOLVER package requirements
-OLD_MINIMUM_ROCBLAS_VERSION="2.41.0"
-NEW_MINIMUM_ROCBLAS_VERSION="2.42.0"
-OLD_MINIMUM_ROCSOLVER_VERSION="3.15.0"
-NEW_MINIMUM_ROCSOLVER_VERSION="3.16.0"
+OLD_MINIMUM_ROCBLAS_VERSION="2.42.0"
+NEW_MINIMUM_ROCBLAS_VERSION="2.43.0"
+OLD_MINIMUM_ROCSOLVER_VERSION="3.16.0"
+NEW_MINIMUM_ROCSOLVER_VERSION="3.17.0"
 sed -i "s/${OLD_MINIMUM_ROCBLAS_VERSION}/${NEW_MINIMUM_ROCBLAS_VERSION}/g" library/CMakeLists.txt
 sed -i "s/${OLD_MINIMUM_ROCSOLVER_VERSION}/${NEW_MINIMUM_ROCSOLVER_VERSION}/g" library/CMakeLists.txt
