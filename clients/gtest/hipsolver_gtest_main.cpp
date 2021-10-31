224ada4ca8c748de0c3314c5fda7521df033651c
Cordell Bloor <Cordell.Bloor@amd.com>
2021-10-30 20:06:13 -0600

    Print version info in hipsolver-test

--- clients/gtest/hipsolver_gtest_main.cpp
+++ clients/gtest/hipsolver_gtest_main.cpp
@@ -1,9 +1,30 @@
 /* ************************************************************************
- * Copyright 2020 Advanced Micro Devices, Inc.
+ * Copyright (c) 2020-2021 Advanced Micro Devices, Inc.
  * ************************************************************************ */
 
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
 #include <gtest/gtest.h>
-#include <stdexcept>
+#include <hipsolver.h>
+
+#include "utility.hpp"
+
+#define STRINGIFY(s) STRINGIFY_HELPER(s)
+#define STRINGIFY_HELPER(s) #s
+
+static void print_version_info()
+{
+    // clang-format off
+    std::cout << "hipSOLVER version "
+        STRINGIFY(hipsolverVersionMajor) "."
+        STRINGIFY(hipsolverVersionMinor) "."
+        STRINGIFY(hipsolverVersionPatch) "."
+        STRINGIFY(hipsolverVersionTweak)
+        << std::endl;
+    // clang-format on
+}
 
 /* =====================================================================
       Main function:
@@ -11,7 +32,20 @@
 
 int main(int argc, char** argv)
 {
+    print_version_info();
+
+    // print device info
+    int device_count = query_device_property();
+    if(device_count <= 0)
+    {
+        std::cerr << "Error: No devices found" << std::endl;
+        return EXIT_FAILURE;
+    }
+    set_device(0); // use first device
+
     ::testing::InitGoogleTest(&argc, argv);
 
-    return RUN_ALL_TESTS();
+    int status = RUN_ALL_TESTS();
+    print_version_info(); // redundant, but convenient when tests fail
+    return status;
 }
