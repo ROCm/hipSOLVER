569cec2389909f8dfe6355e5060645751c25e786
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2022-06-20 13:03:32 -0600

    Fix cuda test failures

--- clients/include/testing_sygvdx_hegvdx.hpp
+++ clients/include/testing_sygvdx_hegvdx.hpp
@@ -1339,9 +1339,9 @@ void testing_sygvdx_hegvdx(Arguments& argus)
     }
 
     // validate results for rocsolver-test
-    // using n * machine_precision as tolerance
+    // using 2 * n * machine_precision as tolerance
     if(argus.unit_check)
-        ROCSOLVER_TEST_CHECK(T, max_error, n);
+        ROCSOLVER_TEST_CHECK(T, max_error, 2 * n);
 
     // output results for rocsolver-bench
     if(argus.timing)
