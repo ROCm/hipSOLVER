2921bd2d632aa0d32a8a11fbadfb4e9e644c3dd5
Jonah Quist <jonahquist@gmail.com>
2022-08-02 17:35:17 -0600

    update copyright information

--- clients/include/utility.hpp
+++ clients/include/utility.hpp
@@ -1,5 +1,24 @@
 /* ************************************************************************
- * Copyright 2020-2022 Advanced Micro Devices, Inc.
+ * Copyright 2020-2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell cop-
+ * ies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM-
+ * PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
+ * CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
  * ************************************************************************ */
 
 #pragma once
@@ -92,10 +111,10 @@ public:
         hipsolverDestroy(m_handle);
     }
 
-    hipsolver_local_handle(const hipsolver_local_handle&) = delete;
-    hipsolver_local_handle(hipsolver_local_handle&&)      = delete;
+    hipsolver_local_handle(const hipsolver_local_handle&)            = delete;
+    hipsolver_local_handle(hipsolver_local_handle&&)                 = delete;
     hipsolver_local_handle& operator=(const hipsolver_local_handle&) = delete;
-    hipsolver_local_handle& operator=(hipsolver_local_handle&&) = delete;
+    hipsolver_local_handle& operator=(hipsolver_local_handle&&)      = delete;
 
     // Allow hipsolver_local_handle to be used anywhere hipsolverHandle_t is expected
     operator hipsolverHandle_t&()
@@ -125,10 +144,10 @@ public:
         hipsolverDnDestroyGesvdjInfo(m_info);
     }
 
-    hipsolver_local_gesvdj_info(const hipsolver_local_gesvdj_info&) = delete;
-    hipsolver_local_gesvdj_info(hipsolver_local_gesvdj_info&&)      = delete;
+    hipsolver_local_gesvdj_info(const hipsolver_local_gesvdj_info&)            = delete;
+    hipsolver_local_gesvdj_info(hipsolver_local_gesvdj_info&&)                 = delete;
     hipsolver_local_gesvdj_info& operator=(const hipsolver_local_gesvdj_info&) = delete;
-    hipsolver_local_gesvdj_info& operator=(hipsolver_local_gesvdj_info&&) = delete;
+    hipsolver_local_gesvdj_info& operator=(hipsolver_local_gesvdj_info&&)      = delete;
 
     // Allow hipsolver_local_gesvdj_info to be used anywhere hipsolverGesvdjInfo_t is expected
     operator hipsolverGesvdjInfo_t&()
@@ -158,10 +177,10 @@ public:
         hipsolverDnDestroySyevjInfo(m_info);
     }
 
-    hipsolver_local_syevj_info(const hipsolver_local_syevj_info&) = delete;
-    hipsolver_local_syevj_info(hipsolver_local_syevj_info&&)      = delete;
+    hipsolver_local_syevj_info(const hipsolver_local_syevj_info&)            = delete;
+    hipsolver_local_syevj_info(hipsolver_local_syevj_info&&)                 = delete;
     hipsolver_local_syevj_info& operator=(const hipsolver_local_syevj_info&) = delete;
-    hipsolver_local_syevj_info& operator=(hipsolver_local_syevj_info&&) = delete;
+    hipsolver_local_syevj_info& operator=(hipsolver_local_syevj_info&&)      = delete;
 
     // Allow hipsolver_local_syevj_info to be used anywhere hipsolverSyevjInfo_t is expected
     operator hipsolverSyevjInfo_t&()
