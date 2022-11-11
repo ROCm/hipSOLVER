dc8025251494e2879b2ee7b5a304787b4d885134
AravindanC <aravindan.cheruvally@amd.com>
2022-11-10 21:07:17 -0800

    Adding SKIP RPATH flag

--- install.sh
+++ install.sh
@@ -584,6 +584,7 @@ fi
     -DCMAKE_SHARED_LINKER_FLAGS="" \
     -DCMAKE_EXE_LINKER_FLAGS="" \
     -DROCM_DISABLE_LDCONFIG=ON \
+    -DCMAKE_SKIP_RPATH=TRUE \
     -DROCM_PATH="${rocm_path}" ../..
   else
     CXX=${compiler} ${cmake_executable} ${cmake_common_options[@]} ${cmake_client_options[@]} -DCPACK_SET_DESTDIR=OFF -DCMAKE_PREFIX_PATH="$(pwd)/../deps/deps-install;${cmake_prefix_path}" -DROCM_PATH=${rocm_path} ../..
