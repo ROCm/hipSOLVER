7e2866b593d0d485a7e5e9ab9f0168fcd934994a
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2024-02-13 17:37:34 -0700

    Implemented param methods for 64-bit API

---
+++ library/include/internal/hipsolver-dense64.h
@@ -0,0 +1,41 @@
+/* ************************************************************************
+ * Copyright (C) 2024 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#ifndef HIPSOLVER_DENSE64_H
+#define HIPSOLVER_DENSE64_H
+
+#include "hipsolver-dense.h"
+#include "hipsolver-types.h"
+
+typedef void* hipsolverDnParams_t;
+
+typedef enum
+{
+    HIPSOLVER_ALG_0 = 231,
+    HIPSOLVER_ALG_1 = 232,
+    HIPSOLVER_ALG_2 = 233,
+} hipsolverAlgMode_t;
+
+typedef enum
+{
+    HIPSOLVERDN_GETRF = 0,
+} hipsolverDnFunction_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+HIPSOLVER_EXPORT hipsolverStatus_t hipsolverDnCreateParams(hipsolverDnParams_t* params);
+
+HIPSOLVER_EXPORT hipsolverStatus_t hipsolverDnDestroyParams(hipsolverDnParams_t params);
+
+HIPSOLVER_EXPORT hipsolverStatus_t hipsolverDnSetAdvOptions(hipsolverDnParams_t   params,
+                                                            hipsolverDnFunction_t func,
+                                                            hipsolverAlgMode_t    alg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HIPSOLVER_DENSE64_H
---
+++ library/src/nvidia_detail/hipsolver_dense64.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************
+ * Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell cop-
+ * ies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM-
+ * PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
+ * CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * ************************************************************************ */
+
+/*! \file
+ *  \brief Implementation of the compatibility APIs that require especial calls
+ *  to hipSOLVER on the cuSOLVER side.
+ */
+
+#include "hipsolver.h"
+
+extern "C" {
+
+hipsolverStatus_t hipsolverDnCreateParams(hipsolverDnParams_t* params)
+try
+{
+    return cuda2hip_status(cusolverDnCreateParams((cusolverDnParams_t*)params));
+}
+catch(...)
+{
+    return exception2hip_status();
+}
+
+hipsolverStatus_t hipsolverDnDestroyParams(hipsolverDnParams_t params)
+try
+{
+    return cuda2hip_status(cusolverDnDestroyParams((cusolverDnParams_t)params));
+}
+catch(...)
+{
+    return exception2hip_status();
+}
+
+hipsolverStatus_t hipsolverDnSetAdvOptions(hipsolverDnParams_t   params,
+                                           hipsolverDnFunction_t func,
+                                           hipsolverAlgMode_t    alg)
+try
+{
+    return cuda2hip_status(cusolverDnSetAdvOptions(
+        (cusolverDnParams_t)params, hip2cuda_function(func), hip2cuda_algmode(alg)));
+}
+catch(...)
+{
+    return exception2hip_status();
+}
+
+} //extern C
