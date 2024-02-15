6d3ed332f23dc63d16a49d4202b139992b305bdc
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2024-02-15 14:56:46 -0700

    Add back hipsolver-compat.h with deprecation

---
+++ library/include/internal/hipsolver-compat.h
@@ -0,0 +1,13 @@
+/* ************************************************************************
+ * Copyright (C) 2020-2024 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#pragma once
+
+#if defined(_MSC_VER)
+#pragma message(": warning: This file is deprecated. Use hipsolver-dense.h instead.")
+#elif defined(__GNUC__)
+#warning "This file is deprecated. Use hipsolver-dense.h instead."
+#endif
+
+#include "hipsolver-dense.h"
