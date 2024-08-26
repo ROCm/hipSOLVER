ff04ceb687d9952ffe597d96d0c22d0586d73bce
Deng <garydeng@amd.com>
2024-08-26 10:50:34 -0600

    Update .jenkins dependencies

--- .jenkins/static.groovy
+++ .jenkins/static.groovy
@@ -15,7 +15,7 @@ def runCI =
 
     def prj  = new rocProject('hipSOLVER', 'StaticLibrary')
     prj.paths.build_command = './install.sh -cd --static -p /opt/rocm/lib/cmake'
-    prj.libraryDependencies = ['rocBLAS', 'rocSPARSE', 'rocSOLVER', 'hipSPARSE', 'rocPRIM']
+    prj.libraryDependencies = ['hipBLAS-common', 'hipBLASLt', 'rocBLAS', 'rocSPARSE', 'rocSOLVER', 'hipSPARSE', 'rocPRIM']
     prj.defaults.ccache = true
 
     // Define test architectures, optional rocm version argument is available
