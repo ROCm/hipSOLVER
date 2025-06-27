4e40203dcba869d73e21155847c2f2133263ef58
John Tyler <jetyler@ualberta.ca>
2025-06-27 13:45:26 -0600

    removed extra modified file

--- clients/gtest/gesvdj_gtest.cpp
+++ clients/gtest/gesvdj_gtest.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2020-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2020-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -145,9 +145,6 @@ class GESVDJ_FORTRAN : public GESVDJ_BASE<API_FORTRAN>
 {
 };
 
-class GESVDJ_COMPAT : public GESVDJ_BASE<API_COMPAT>
-{
-};
 // non-batch tests
 
 TEST_P(GESVDJ, __float)
@@ -190,23 +187,6 @@ TEST_P(GESVDJ_FORTRAN, __double_complex)
     run_tests<false, false, rocblas_double_complex>();
 }
 
-TEST_P(GESVDJ_COMPAT, __float)
-{
-    run_tests<false, false, float>();
-}
-
-TEST_P(GESVDJ_COMPAT, __double)
-{
-    run_tests<false, false, double>();
-}
-
-TEST_P(GESVDJ_COMPAT, __float_complex)
-{
-    run_tests<false, false, rocblas_float_complex>();
-}
-
-TEST_P(GESVDJ_COMPAT, __double_complex)
-{
     run_tests<false, false, rocblas_double_complex>();
 }
 
@@ -287,10 +267,3 @@ INSTANTIATE_TEST_SUITE_P(checkin_lapack,
                          GESVDJ_FORTRAN,
                          Combine(ValuesIn(size_range), ValuesIn(opt_range)));
 
-// INSTANTIATE_TEST_SUITE_P(daily_lapack,
-//                          GESVDJ_COMPAT,
-//                          Combine(ValuesIn(large_size_range), ValuesIn(large_opt_range)));
-
-INSTANTIATE_TEST_SUITE_P(checkin_lapack,
-                         GESVDJ_COMPAT,
-                         Combine(ValuesIn(size_range), ValuesIn(opt_range)));
