232d2698328a105af1086455cec2ee23e9a554a5
John Tyler <jetyler@ualberta.ca>
2025-06-27 13:47:16 -0600

    fully reverted file to old commit

--- clients/gtest/gesvdj_gtest.cpp
+++ clients/gtest/gesvdj_gtest.cpp
@@ -187,9 +187,6 @@ TEST_P(GESVDJ_FORTRAN, __double_complex)
     run_tests<false, false, rocblas_double_complex>();
 }
 
-    run_tests<false, false, rocblas_double_complex>();
-}
-
 // strided_batched tests
 
 TEST_P(GESVDJ, strided_batched__float)
@@ -232,25 +229,6 @@ TEST_P(GESVDJ_FORTRAN, strided_batched__double_complex)
     run_tests<false, true, rocblas_double_complex>();
 }
 
-TEST_P(GESVDJ_COMPAT, strided_batched__float)
-{
-    run_tests<false, true, float>();
-}
-
-TEST_P(GESVDJ_COMPAT, strided_batched__double)
-{
-    run_tests<false, true, double>();
-}
-
-TEST_P(GESVDJ_COMPAT, strided_batched__float_complex)
-{
-    run_tests<false, true, rocblas_float_complex>();
-}
-
-TEST_P(GESVDJ_COMPAT, strided_batched__double_complex)
-{
-    run_tests<false, true, rocblas_double_complex>();
-}
 // INSTANTIATE_TEST_SUITE_P(daily_lapack,
 //                          GESVDJ,
 //                          Combine(ValuesIn(large_size_range), ValuesIn(large_opt_range)));
@@ -266,4 +244,3 @@ INSTANTIATE_TEST_SUITE_P(checkin_lapack,
 INSTANTIATE_TEST_SUITE_P(checkin_lapack,
                          GESVDJ_FORTRAN,
                          Combine(ValuesIn(size_range), ValuesIn(opt_range)));
-
