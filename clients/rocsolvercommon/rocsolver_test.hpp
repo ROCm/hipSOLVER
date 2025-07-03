d1f5e4bc4751d7bff0f8f5381d73a48adec84be1
John <121990449+john00003@users.noreply.github.com>
2025-07-03 15:08:54 -0600

    undo formatting

--- clients/rocsolvercommon/rocsolver_test.hpp
+++ clients/rocsolvercommon/rocsolver_test.hpp
@@ -60,7 +60,7 @@ constexpr double get_safemin()
 }
 
 #ifdef GOOGLE_TEST
-#define ROCSOLVER_TEST_CHECK(T, max_error, tol) ASSERT_LE((max_error), (tol) * get_epsilon<T>())
+#define ROCSOLVER_TEST_CHECK(T, max_error, tol) ASSERT_LE((max_error), (tol)*get_epsilon<T>())
 #else
 #define ROCSOLVER_TEST_CHECK(T, max_error, tol)
 #endif
